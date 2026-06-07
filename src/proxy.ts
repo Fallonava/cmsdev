72ea3e0840092f385df6991095bb93e60e9efaf3
FaishalFx <238270619+Fallonava@users.noreply.github.com>
2026-06-07 07:15:56 +0700

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-export const { auth: proxy } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+export { auth as proxy };
 
 export const config = {
   // Matcher ignoring /api, /_next/static, /_next/image, and all files with an extension (e.g. .png, .css)
